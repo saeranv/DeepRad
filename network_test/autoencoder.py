5ace4a53afea33cb03d0e60f726617062291651a
saeranv <saeranv@gmail.com>
2021-04-27 20:24:59 -0400

    feat(autoencoder.py): change color mapping.

--- network_test/autoencoder.py
+++ network_test/autoencoder.py
@@ -8,15 +8,19 @@ import numpy as np
 
 from torch.utils.data import Dataset, DataLoader
 from PIL import *
+import utils
 
 import os
+
 import glob
 import math
 import random
 import re
 
-np.random.seed(2)
 
+RADCMAP = plt.get_cmap('RdYlBu_r')
+
+np.random.seed(2)
 
 #############################
 #### Reduce Image Size ######
@@ -28,19 +32,20 @@ org_folder_path = "load_test"
 org_folder_pathc1 = "load_test_c1"
 org_folder_pathc2 = "load_test_c2"
 
+
 def reduce_img(path, train_path, test_path):
     image_list = []
     for i, filename in enumerate(glob.glob(path + '/*.jpg')):
-    
+
         if filename.lower().endswith('.jpg'):
             g = filename.split('.')[0]
-   
-            img=Image.open(filename).convert("RGB")
+
+            img = Image.open(filename).convert("RGB")
             new_name = filename.split('/')[1]
 
             img_c1 = Image.open(filename).convert("RGB")
-          
-            w,h = img.size 
+
+            w, h = img.size
             # print(w,h)
             # img = Image.open(filename)
             border = 230
@@ -48,14 +53,14 @@ def reduce_img(path, train_path, test_path):
             y = 230
             w_ = 1328
             h_ = 240
-            img = img.crop((x,y,x+w_,y+h_))
-            new_img = img.resize((math.ceil(w *.3), int(h*.3)))
-            if i % 7 == 0: 
-                new_img.save(test_path + str(i) + "_" + new_name, optimize=True)
+            img = img.crop((x, y, x + w_, y + h_))
+            new_img = img.resize((math.ceil(w * .3), int(h * .3)))
+            if i % 7 == 0:
+                new_img.save(test_path + str(i) + "_" +
+                             new_name, optimize=True)
             else:
                 new_img.save(train_path + new_name, optimize=True)
-                
-  
+
 
 # reduce_img(org_folder_path, 'small_train/', 'small_test/')
 # reduce_img(org_folder_pathc1, 'small_train_c1/', 'small_test_c1/')
@@ -66,11 +71,10 @@ def reduce_img(path, train_path, test_path):
 #############################
 
 
-
 class Autoencoder(nn.Module):
     def __init__(self):
         super(Autoencoder, self).__init__()
-        self.encoder = nn.Sequential( # like the Composition layer you built
+        self.encoder = nn.Sequential(  # like the Composition layer you built
             nn.Conv2d(6, 16, 3, stride=2, padding=1),
             nn.ReLU(),
             nn.Conv2d(16, 32, 3, stride=2, padding=1),
@@ -81,9 +85,11 @@ class Autoencoder(nn.Module):
         self.decoder = nn.Sequential(
             nn.ConvTranspose2d(64, 32, 7),
             nn.ReLU(),
-            nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(32, 16, 3, stride=2,
+                               padding=1, output_padding=1),
             nn.ReLU(),
-            nn.ConvTranspose2d(16, 3, 3, stride=2, padding=1, output_padding=1),
+            nn.ConvTranspose2d(16, 3, 3, stride=2,
+                               padding=1, output_padding=1),
             # nn.Sigmoid()
             # nn.Tanh
         )
@@ -99,7 +105,7 @@ class Autoencoder(nn.Module):
 #############################
 
 class CustomDataSet(Dataset):
-    def __init__(self, main_dir,c1_dir, c2_dir, transform):
+    def __init__(self, main_dir, c1_dir, c2_dir, transform):
         self.main_dir = main_dir
         self.c1_dir = c1_dir
         self.c2_dir = c2_dir
@@ -113,8 +119,6 @@ class CustomDataSet(Dataset):
         c1_imgs = self.sort_img(c1_imgs)
         c2_imgs = self.sort_img(c2_imgs)
 
-
-
         self.total_imgs = all_imgs
         self.c1_imgs = c1_imgs
         self.c2_imgs = c2_imgs
@@ -126,14 +130,13 @@ class CustomDataSet(Dataset):
             k = i.split('_')[0]
             keys.append(int(k))
             names.append(i)
-        
+
         _, sorted_list = (list(t) for t in zip(*sorted(zip(keys, names))))
         # print(sorted_list[:5])
         return sorted_list
 
     def __len__(self):
         return len(self.total_imgs)
-
 
     def __getitem__(self, idx):
         img_loc = os.path.join(self.main_dir, self.total_imgs[idx])
@@ -144,25 +147,27 @@ class CustomDataSet(Dataset):
         image_c1 = Image.open(img_loc_c1).convert("RGB")
         image_c2 = Image.open(img_loc_c2).convert("RGB")
 
-        x = torch.cat((self.transform(image_c1), self.transform(image_c2)), dim=0)
+        x = torch.cat((self.transform(image_c1),
+                       self.transform(image_c2)), dim=0)
 
         y = self.transform(image)
         return x, y
 
 
-## Train Data folders
+# Train Data folders
 img_folder_path = "small_train"
 c1_folder_path = "small_train_c1"
 c2_folder_path = "small_train_c2"
 
-## Test Data Folders
+# Test Data Folders
 img_folder_path_test = "small_test"
 c1_folder_path_test = "small_test_c1"
 c2_folder_path_test = "small_test_c2"
 
-train_data = CustomDataSet(img_folder_path, c1_folder_path, c2_folder_path, transform=transforms.ToTensor())
-test_data = CustomDataSet(img_folder_path_test, c1_folder_path_test, c2_folder_path_test, transform=transforms.ToTensor())
-
+train_data = CustomDataSet(img_folder_path, c1_folder_path,
+                           c2_folder_path, transform=transforms.ToTensor())
+test_data = CustomDataSet(img_folder_path_test, c1_folder_path_test,
+                          c2_folder_path_test, transform=transforms.ToTensor())
 
 
 #########################################
@@ -172,18 +177,18 @@ test_data = CustomDataSet(img_folder_path_test, c1_folder_path_test, c2_folder_p
 
 def train(model, num_epochs=5, batch_size=4, learning_rate=1e-3):
     torch.manual_seed(42)
-    criterion = nn.MSELoss() # mean square error loss
+    criterion = nn.MSELoss()  # mean square error loss
 
     optimizer = torch.optim.Adam(model.parameters(),
-                                 lr=learning_rate, 
-                                 weight_decay=1e-5) # <--
+                                 lr=learning_rate,
+                                 weight_decay=1e-5)  # <--
 
-    train_loader = DataLoader(train_data , batch_size=batch_size, shuffle=True)
-    
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+
     outputs = []
     for epoch in range(num_epochs):
         for data in train_loader:
-            
+
             img, targ = data
 
             recon = model.forward(img)
@@ -193,15 +198,24 @@ def train(model, num_epochs=5, batch_size=4, learning_rate=1e-3):
             optimizer.step()
             optimizer.zero_grad()
 
-        print('Epoch:{}, Loss:{:.4f}'.format(epoch+1, float(loss)))
+        print('Epoch:{}, Loss:{:.4f}'.format(epoch + 1, float(loss)))
         # outputs.append((epoch, img, recon),)
         outputs.append((epoch, targ, recon),)
     return outputs
-    
+
+
+def color2rad(img):
+    """img is np.ndarray of floats btwn 0 - 1"""
+    img = (img * 255).astype(np.uint8)
+    # TODO: add a mask here
+    img = np.where(img < (255 - 1e-10), img, np.nan)
+    return img
+
 
 model = Autoencoder()
-max_epochs = 30
+max_epochs = 1  # 30
 outputs = train(model, num_epochs=max_epochs)
+
 # print(len(outputs))
 for k in range(0, max_epochs, 5):
     # print(k)
@@ -209,30 +223,39 @@ for k in range(0, max_epochs, 5):
     plt.figure(figsize=(9, 2))
     imgs = outputs[k][1].detach().numpy()
     recon = outputs[k][2].detach().numpy()
-    # print(len(imgs))
     for i, item in enumerate(imgs):
-        if i >= 9: break
-        plt.subplot(2, 9, i+1)
-        plt.imshow(item[0], cmap='jet')
+        if i >= 9:
+            break
+        plt.subplot(2, 9, i + 1)
+        img = item[0]
+        plt.imshow(color2rad(img), cmap=RADCMAP, vmin=0, vmax=255)
+        # imgfpath = os.path.join(os.getcwd(), 'img_{}.jpg'.format(k))
+        # utils.write_img(img, imgfpath)
+        # print(len(imgs))
 
-        
-    for i, item in enumerate(recon):   
-        if i >= 9: break
-        plt.subplot(2, 9, 9+i+1)
-        plt.imshow(item[0], cmap = 'jet')
+    for i, item in enumerate(recon):
+        if i >= 9:
+            break
+        plt.subplot(2, 9, 9 + i + 1)
+        img = item[0]
+        img = item[0]
+        plt.imshow(color2rad(img), cmap=RADCMAP, vmin=0, vmax=255)
+        #imgfpath = os.path.join(os.getcwd(), 'recon_{}.jpg'.format(i))
+        #utils.write_img(item[0], imgfpath)
+
     plt.show()
-
-
 #########################################
 ############ Test Model #################
 #########################################
 print('Viewing Test Images')
+
+
 def test(model, batch_size=64, learning_rate=1e-3):
     torch.manual_seed(42)
 
-    test_loader = torch.utils.data.DataLoader(test_data, 
-                                               batch_size=batch_size, 
-                                               shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_data,
+                                              batch_size=batch_size,
+                                              shuffle=True)
     outputs = []
 
     for data in test_loader:
@@ -245,19 +268,21 @@ def test(model, batch_size=64, learning_rate=1e-3):
 
 outputs_test = test(model)
 
-
 for k in range(0, len(outputs_test), 5):
     plt.figure(figsize=(9, 2))
     imgs = outputs_test[k][0].detach().numpy()
     recon = outputs_test[k][1].detach().numpy()
     for i, item in enumerate(imgs):
-        if i >= 9: break
-        plt.subplot(2, 9, i+1)
-        plt.imshow(item[0])
-
-        
+        if i >= 9:
+            break
+        plt.subplot(2, 9, i + 1)
+        img = item[0]
+        plt.imshow(color2rad(img), cmap=RADCMAP, vmin=0, vmax=255)
     for i, item in enumerate(recon):
-        if i >= 9: break
-        plt.subplot(2, 9, 9+i+1)
-        plt.imshow(item[0])
+        if i >= 9:
+            break
+        plt.subplot(2, 9, 9 + i + 1)
+        img = item[0]
+        plt.imshow(color2rad(img), cmap=RADCMAP, vmin=0, vmax=255)
+
     plt.show()
---
+++ network_test/utils.py
@@ -0,0 +1,166 @@
+"""Utility functions for deeprad."""
+import sys
+import os
+import json
+from pathlib import Path
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+from typing import List
+import shapely.geometry as geom
+from pprint import pprint
+
+# Path to all models in deep_rad
+DEEPRAD_MODELS_DIR = os.path.abspath(os.path.join(
+    os.getcwd(), '..', 'deeprad/data/models/'))
+DEEPRAD_GHOUT_DIR = os.path.abspath(os.path.join(
+    os.getcwd(), '..', 'deeprad/data/ghout/'))
+DEEPRAD_TRAINTEST_DIR = os.path.abspath(os.path.join(
+    os.getcwd(), '..', 'deeprad/data/traintest/'))
+
+
+def pp(x, *args):
+    pprint(x) if not args else print(x, *args)
+
+
+def fd(module, key=None):
+    """ To efficiently search modules."""
+    def hfd(m, k): return k.lower() in m.lower()
+    if key is None:
+        return [m for m in dir(module)][::-1]
+    else:
+        return [m for m in dir(module) if hfd(m, key)][::-1]
+
+
+def to_poly_sh(xy_arr):
+    """Shapely polygon from list of two arrays of x, y coordinates.
+
+        Args:
+            xy_arr: [x_arr, y_arr]
+    Example:
+        to_poly_sh(to_poly_np(poly_sh))
+        to_poly_np(to_poly_sh(poly_np))
+    """
+    return geom.Polygon([(x, y) for x, y in zip(*xy_arr)])
+
+
+def to_poly_np(poly_sh):
+    """Two arrays of x, y coordinates from shapely polygon.
+
+    Example:
+        to_poly_sh(to_poly_np(poly_sh))
+        to_poly_np(to_poly_sh(poly_np))
+    """
+    return np.array(poly_sh.exterior.xy)
+
+
+def load_img_gray(img_fpath: str) -> np.ndarray:
+    return cv2.imread(img_fpath, cv2.IMREAD_GRAYSCALE)
+
+
+def load_img_rgb(img_fpath: str) -> np.ndarray:
+    return cv2.imread(img_fpath, cv2.COLOR_BGR2RGB)
+
+
+def write_img(img: np.ndarray, img_fpath: str) -> bool:
+    return cv2.imwrite(img_fpath, img)
+
+
+def extract_floorplan_ids(data_num, target_data_dir=None, verbose=True):
+    """Safely extract root model directories for polygon extraction."""
+
+    if target_data_dir is None:
+        target_data_dir = DEEPRAD_MODELS_DIR
+
+    # Load all model directories
+    floorplan_id_arr = os.listdir(target_data_dir)
+    n_ids = len(floorplan_id_arr)
+
+    if n_ids == 0:
+        raise Exception('No image files. Add images and try again.')
+
+    if data_num > n_ids:
+        if verbose:
+            print('Note: data_num {} is too high. Resetting to n_ids {}.'.format(
+                  data_num, n_ids))
+        data_num = n_ids
+
+    return data_num, floorplan_id_arr
+
+
+def load_json(json_fpath):
+    with open(json_fpath, 'r') as fp:
+        val_dict = json.load(fp)
+
+    return val_dict
+
+
+def load_floorplan_data(targ_id_dirs, data_num):
+    """Load floorplan data."""
+
+    src_img_arr = [0] * data_num
+    label_img_arr = [0] * data_num
+    hdict_arr = [0] * data_num
+    targ_id_dir_arr = [0] * data_num
+
+    i = -1
+    idx = -1
+    total_i = 0
+    null_lst = []
+
+    while (idx + 1) < data_num:
+        i += 1
+        targ_id_dir = os.path.join(
+            DEEPRAD_MODELS_DIR, targ_id_dirs[i], 'data')
+        targ_src_fpath = os.path.join(targ_id_dir, 'src.jpg')
+        targ_label_fpath = os.path.join(targ_id_dir, 'label.jpg')
+        targ_json_fpath = os.path.join(targ_id_dir, 'door_vecs.json')
+
+        hdict = load_json(targ_json_fpath)
+
+        # if hdict['scale'] < 0.4:
+        #     print('Skip {} b/c scale at {}. Total skipped={}.'.format(
+        #           targ_id_dirs[i], hdict['scale'], total_i))
+        #     total_i += 1
+        #     continue
+
+        idx += 1
+        hdict_arr[idx] = hdict
+        src_img_arr[idx] = load_img_rgb(targ_src_fpath)
+        label_img_arr[idx] = load_img_gray(targ_label_fpath)
+        targ_id_dir_arr[idx] = targ_id_dir
+        null_lst.append(targ_id_dirs[i] + '\n')
+
+    # Write to null list
+    null_fpath = os.path.join(DEEPRAD_MODELS_DIR, '_null.txt')
+    with open(null_fpath, 'w') as fp:
+        # [fp.writeline(null_) for null_ in null_lst]
+        fp.writelines(null_lst)
+    return hdict_arr, src_img_arr, label_img_arr, targ_id_dir_arr
+
+
+def make_dir_safely(dest_dir):
+    Path(dest_dir).mkdir(parents=True, exist_ok=True)
+
+
+def fname_from_fpath(fpath):
+    """Splits filename from extension and preceding directories."""
+    return Path(fpath).stem
+
+
+def to_multi_channel_img_arr(imgs: List[np.ndarray]):
+    """Safely constructs a multichannel array from multiple grayscale images."""
+
+    n_channels = len(imgs)
+
+    assert np.all([np.array_equal([len(img.shape)], [2])
+                  for img in imgs]), 'img must be 2d grayscale.'
+
+    assert np.all([np.array_equal(img.shape, imgs[0].shape) for img in imgs]), \
+        'All images must be same shape. Got {}.'.format(
+            [img.shape for img in imgs])
+
+    cat_img = np.dstack(imgs)  # Concat depthwise channels
+    assert cat_img.shape[2] == n_channels
+
+    return cat_img
